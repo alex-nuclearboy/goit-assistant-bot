78b4f2ce0ee652388b127bb4517ba8f47fb85fb0
Aleksander <alex.nuclearboy@gmail.com>
2023-12-15 15:34:19 +0100

    add a script

---
+++ assistant_bot.py
@@ -0,0 +1,77 @@
+def input_error(func):
+
+    def error_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Enter a valid user name."
+        except ValueError:
+            return "Please provide both name and phone number."
+        except IndexError:
+            return "Invalid input format."
+
+    return error_func
+
+
+@input_error
+def add_contact(data, phonebook):
+    name, number = data.split(" ", 1)
+    if name not in phonebook:
+        phonebook[name] = number
+        return f"Contact {name} added."
+    else:
+        return f"Contact {name} already exists. For updating, use command 'change'"
+
+
+@input_error
+def change_contact(data, phonebook):
+    name, number = data.split(" ", 1)
+    if name in phonebook:
+        phonebook[name] = number
+        return f"Contact {name} updated."
+    else:
+        return "Contact not found."
+
+
+@input_error
+def show_phone(name, phonebook):
+    return phonebook[name]
+
+
+@input_error
+def show_all(phonebook):
+    return "\n".join(
+        [f"{name}: {number}" for name, number in phonebook.items()])
+
+
+def main():
+    phonebook = {}
+    commands = {
+        "hello": lambda _: "Hello! How can I help you?",
+        "add": lambda data: add_contact(data, phonebook),
+        "change": lambda data: change_contact(data, phonebook),
+        "phone": lambda data: show_phone(data, phonebook),
+        "show all": lambda _: show_all(phonebook),
+        "good bye": lambda _: "Good bye!",
+        "close": lambda _: "Good bye!",
+        "exit": lambda _: "Good bye!"
+    }
+
+    while True:
+        input_data = input("Enter command: ")
+        if input_data.lower() in ["good bye", "close", "exit"]:
+            print(commands[input_data.lower()](None))
+            break
+        elif input_data.lower() in ["hello", "show all"]:
+            print(commands[input_data.lower()](None))
+        else:
+            command, *data = input_data.split(maxsplit=1)
+            data = " ".join(data)
+            if command.lower() in commands:
+                print(commands[command.lower()](data))
+            else:
+                print("Unknown command. Try again.")
+
+
+if __name__ == "__main__":
+    main()
